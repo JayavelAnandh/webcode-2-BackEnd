862b3b64cc0c4a7378006b21865c733833ab6ba9
JayavelAnandh <jvanandh984@gmail.com>
2023-03-15 12:07:10 +0530

    Editing Schemas

--- routes/showRoutes.js
+++ routes/showRoutes.js
@@ -6,14 +6,17 @@ const router = express.Router();
 router.get("/",async(req,res)=>{
     
     try {
-        // if(req.query){
-        //     const currentQuery = req.query
-        //     const response= await ShowSchema.find({currentQuery})
-        //     return  res.status(200).send(response)
-        // }
-        const response= await ShowSchema.find({theatreId:req.body.theatreId})
-        console.log(res)
-       return  res.status(200).send(response)
+        if(req.query){
+            const currentQuery = req.query.theatreId
+            const response= await ShowSchema.find({theatreId:currentQuery})
+            console.log(response)
+            return  res.status(200).send(response)
+        }
+        
+        if(!req.query){
+            return  res.status(400).send("Add a query")
+        }
+       
     } catch (error) {
         res.status(500).send(error)
     }
