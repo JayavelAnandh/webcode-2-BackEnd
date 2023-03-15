2234e9281028480d86473273646a456c0f3ef14d
JayavelAnandh <jvanandh984@gmail.com>
2023-03-15 11:48:16 +0530

    Editing Schemas

--- routes/showRoutes.js
+++ routes/showRoutes.js
@@ -4,17 +4,16 @@ import { ShowSchema } from "../models/showSchema.js";
 const router = express.Router();
 
 router.get("/",async(req,res)=>{
+    
     try {
-        if(req.query){
-            const givenQuery = req.query
-            const findShow = await ShowSchema.find({givenQuery})
-            return res.status(200).send(findShow)
-        }
-        if(!req.query){
-            const findShow = await ShowSchema.find({})
-            return res.status(200).send(findShow)
-        }
-       
+        // if(req.query){
+        //     const currentQuery = req.query
+        //     const response= await ShowSchema.find({currentQuery})
+        //     return  res.status(200).send(response)
+        // }
+        const response= await ShowSchema.find({theatreId:req.body.theatreId})
+        console.log(res)
+       return  res.status(200).send(response)
     } catch (error) {
         res.status(500).send(error)
     }
